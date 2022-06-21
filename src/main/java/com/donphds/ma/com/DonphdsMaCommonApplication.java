487cc02815331bfa234e18c8105798832675c0df
donphds <donphds@163.com>
2022-06-21 19:43:56 +0800

    Initial commit

---
+++ src/main/java/com/donphds/ma/com/DonphdsMaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.donphds.ma.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonphdsMaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonphdsMaCommonApplication.class, args);
+    }
+
+}
